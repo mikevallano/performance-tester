2bf08c6764861ec71df8da175d280b6ba120c7d7
mike vallano <mikevallano@gmail.com>
2018-10-15 18:44:58 -0500

    Add product category.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181014160120) do
+ActiveRecord::Schema.define(version: 20181015232906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 20181014160120) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["customer_company_id"], name: "index_customers_on_customer_company_id"
+  end
+
+  create_table "product_categories", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
