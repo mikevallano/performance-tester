5d9259b1ddb0a8e7af2fead028acd70bb23e1e9d
mike vallano <mikevallano@gmail.com>
2018-10-16 06:22:23 -0500

    Add relationships for products and product categories

--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -1,5 +1,9 @@
 class ProductCategory < ApplicationRecord
   has_many :products
+  has_many :product_order_items, through: :products
+  has_many :orders, through: :products
+  has_many :customers, through: :products
+  has_many :customer_companies, through: :products
 
   validates :name, presence: true
 end
