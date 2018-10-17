22c24a4e6d1cb8a9ae874e40275dda7335d7b400
mike vallano <mikevallano@gmail.com>
2018-10-16 19:22:23 -0500

    Add relationships to customer company.

--- spec/models/customer_company_spec.rb
+++ spec/models/customer_company_spec.rb
@@ -12,4 +12,14 @@ RSpec.describe CustomerCompany, type: :model do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:tax_id) }
   end
+
+  describe 'relationships' do
+    it { is_expected.to have_many(:customers) }
+    it { is_expected.to have_many(:orders) }
+    it { is_expected.to have_many(:product_order_items) }
+    it { is_expected.to have_many(:salesreps) }
+    it { is_expected.to have_many(:sales_divisions) }
+    it { is_expected.to have_many(:products) }
+    it { is_expected.to have_many(:product_categories) }
+  end
 end
