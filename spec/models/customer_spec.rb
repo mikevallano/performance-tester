1ea54b78f57e9e92839ad4b4066e4b88e03ad3d0
mike vallano <mikevallano@gmail.com>
2018-10-16 19:17:22 -0500

    Add relationships for customers.

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -17,4 +17,14 @@ RSpec.describe Customer, type: :model do
     it { is_expected.to validate_presence_of(:last_name) }
     it { is_expected.to validate_presence_of(:phone) }
   end
+
+  describe 'relationships' do
+    it { is_expected.to belong_to(:customer_company) }
+    it { is_expected.to have_many(:orders) }
+    it { is_expected.to have_many(:product_order_items) }
+    it { is_expected.to have_many(:salesreps) }
+    it { is_expected.to have_many(:sales_divisions) }
+    it { is_expected.to have_many(:products) }
+    it { is_expected.to have_many(:product_categories) }
+  end
 end
